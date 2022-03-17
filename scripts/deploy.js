91068d84955cd6b624b6ca0ebba19f8305bb75a3
zendus <uzendujmike@gmail.com>
2022-03-17 04:13:01 +0100

    feat: verified erc721

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,11 +5,11 @@ const main = async () => {
   console.log("Deploying contracts with account: ", deployer.address);
   console.log("Account balance: ", accountBalance.toString());
 
-  //   const erc20ContractFactory = await hre.ethers.getContractFactory("ERC20");
-  //   const erc20Contract = await erc20ContractFactory.deploy("Zendus Token", "ZT");
-  //   await erc20Contract.deployed();
+  const erc20ContractFactory = await hre.ethers.getContractFactory("ERC20");
+  const erc20Contract = await erc20ContractFactory.deploy("Zendus Token", "ZT");
+  await erc20Contract.deployed();
 
-  //   console.log("ERC20 address: ", erc20Contract.address);
+  console.log("ERC20 address: ", erc20Contract.address);
 
   const erc721ContractFactory = await hre.ethers.getContractFactory(
     "ZendusNFT"
